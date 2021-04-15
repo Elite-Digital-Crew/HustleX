6e5575983cf9973b6ccdfe74667fff17cc63b85d
ChinmayX <chinmaysjr26@gmail.com>
2021-04-15 08:37:27 +0530

    colours working

--- App.js
+++ App.js
@@ -2,6 +2,8 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+import colours from './src/assets/colours/colours';
+
 export default function App() {
   return (
     <View style={styles.container}>
@@ -14,7 +16,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: colours.primary ,
     alignItems: 'center',
     justifyContent: 'center',
   },
