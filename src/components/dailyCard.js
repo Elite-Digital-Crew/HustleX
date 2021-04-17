5a814b145244d14c8b2277bee56355903285f924
ChinmayX <chinmaysjr26@gmail.com>
2021-04-17 15:05:13 +0530

    pie chart issues fixed

--- src/components/dailyCard.js
+++ src/components/dailyCard.js
@@ -2,27 +2,37 @@ import React from 'react'
 import { View, Text, FlatList, StyleSheet } from 'react-native';
 import colours from '../assets/colours/colours';
 import { heightToDp, widthToDp } from '../Utils';
-import PieChart from '../components/pieChart'
+import PieChart from './piechart';
 
 const dailyCard = ({data}) => {
     return (
         <View style={{
             height: heightToDp('25%'),
             borderRadius: widthToDp('4%'),
+            // width: widthToDp('94%'),
+            alignItems: 'center',
             backgroundColor: colours.grey,
             flexDirection: 'row'
         }}>
             <View style={{
                 width: '50%',
-                height: '100%',
-                justifyContent:'center',
+                height: '70%',
+                justifyContent: 'space-around',
                 alignItems: 'center',
             }}>
-                <View>
+                <View style={{
+                    width: '50%',
+                    borderLeftColor: colours.secondary,
+                    borderLeftWidth: widthToDp('1%'),
+                }}>
                     <Text style={styles.h2}>STEPS</Text>
                     <Text style={styles.h2}>{data.steps}</Text>
                 </View>
-                <View>
+                <View style={{
+                    width: '50%',
+                    borderLeftColor: colours.primary,
+                    borderLeftWidth: widthToDp('1%'),
+                }}>
                     <Text style={styles.h2}>KACL</Text>
                     <Text style={styles.h2}>{data.kcal}</Text>
                 </View>
@@ -33,7 +43,7 @@ const dailyCard = ({data}) => {
                 justifyContent:'center',
                 alignItems: 'center',
             }}>
-                <PieChart percentage={data.steps} max={data.stepsTarget} color={colours.primary} radius={widthToDp('12%')} />
+                <PieChart />
             </View>
         </View>
     )
