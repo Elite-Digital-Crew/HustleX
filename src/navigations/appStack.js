d2d3ab748ba454094d11ef97e9085b83e26f8738
ChinmayX <chinmaysjr26@gmail.com>
2021-04-18 20:34:21 +0530

    icons in tab navigator

--- src/navigations/appStack.js
+++ src/navigations/appStack.js
@@ -1,9 +1,13 @@
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
+import { Ionicons } from "@expo/vector-icons";
+
 import React from 'react';
 //importing Auth screens
 import Home from '../screens/Home/index';
 import Blog from '../screens/Blog/index';
+import colours from '../assets/colours/colours';
+import { widthToDp, heightToDp } from '../Utils';
 
 
 //createing stack navigator
@@ -12,9 +16,34 @@ const AppStack = createBottomTabNavigator();
 const AppIndex = () => {
     return (
     <AppStack.Navigator
-    screenOptions={{
-    headerShown: false
-    }}>
+    screenOptions={({ route }) => ({
+        tabBarIcon: ({ focused, color, size }) => {
+          let iconName;
+
+          if (route.name === 'Home') {
+            iconName = focused
+              ? 'home'
+              : 'home-outline';
+          }else if (route.name === 'Cources') {
+            iconName = focused ? 'barbell' : 'barbell-outline';
+          } 
+          else if (route.name === 'Blog') {
+            iconName = focused ? 'newspaper' : 'newspaper-outline';
+          }
+          // You can return any component that you like here!
+          return <Ionicons name={iconName} size={size} color={color} />;
+        },
+      })}
+    tabBarOptions={{
+    activeTintColor: colours.text,
+    inactiveTintColor: colours.darkgrey,
+    style: {
+        backgroundColor: colours.grey,
+        borderTopEndRadius: widthToDp('4%'),
+        borderTopStartRadius: widthToDp('4%'),
+        borderColor: colours.grey,
+        height: heightToDp('6%'),
+    }}}>
         <AppStack.Screen name="Home" component={Home}/>
         <AppStack.Screen name="Blog" component={Blog}/>
     </AppStack.Navigator>
