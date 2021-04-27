08061a7f00e54cfdcda16b768e9cf4f3e5a1e807
ChinmayX <chinmaysjr26@gmail.com>
2021-04-27 20:04:22 +0530

    updates

---
+++ src/components/textInput.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import { Text, TouchableOpacity } from 'react-native';
+import colours from '../assets/colours/colours';
+import { widthToDp } from '../Utils';
+
+const textInput = ({
+    text = "text",
+    colour = "tomato",
+    height = 80,
+    width = 250,
+}) => {
+    return (
+        <TouchableOpacity style={{
+            height: height,
+            width : width,
+            backgroundColor : colour,
+            justifyContent: 'center', 
+            alignItems: 'center',
+            borderRadius: 100
+        }}
+        onPress={onPress}>
+            <Text style={{
+                fontFamily: 'PoppinsBold',
+                fontSize: widthToDp('5%'),
+                color: colours.white,
+            }}>{text}</Text>
+        </TouchableOpacity>  
+    )
+}
+
+export default textInput
--- src/screens/Home/home.js
+++ src/screens/Home/home.js
@@ -1,16 +1,23 @@
 import React from 'react';
-import { Text, View, TouchableOpacity, TextInput, SafeAreaView, ScrollView } from 'react-native';
-import colours from '../../assets/colours/colours';
+import { Text, View } from 'react-native';
 
+// styling components
+import colours from '../../assets/colours/colours';
+import { heightToDp, widthToDp } from '../../Utils';
+
+// importing components 
+import Button from '../../components/button'
+import Landing from '../Auth/createProflie'
 
 const Home = ({ navigation }) => {
+ 
     return (
         <View style={{
             flex: 1,
             justifyContent: 'center',
             alignItems: 'center'
         }}>
-            <Text>Home</Text>
+            <Landing />
         </View>
     );
 }
