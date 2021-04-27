99cfea18782c99362a595ce95e4ad296fe539f35
ChinmayX <chinmaysjr26@gmail.com>
2021-04-27 19:52:02 +0530

    Create login.js

---
+++ src/screens/Auth/login.js
@@ -0,0 +1,117 @@
+import React from 'react';
+import { Text, View, TextInput, TouchableOpacity, Alert } from 'react-native';
+
+// styling components
+import colours from '../../assets/colours/colours';
+import { heightToDp, widthToDp } from '../../Utils';
+
+// icons
+import { AntDesign } from '@expo/vector-icons';
+import { FontAwesome5 } from '@expo/vector-icons';
+
+// importing components 
+import Button from '../../components/button'
+
+const Landing = ({ navigation }) => {
+
+    function navigateLogin() {
+        navigation.navigate("Login")
+    } 
+    return (
+        <View style={{
+            height: heightToDp('65%'),
+            justifyContent: 'space-around',
+            alignItems: 'center'
+        }}>
+
+            {/* hedder text */}
+            <Text style={{
+                fontFamily: "PoppinsBold",
+                fontSize: widthToDp('14%'),
+                color: colours.text,
+            }}>LOGIN</Text>
+            <View style={{
+                height: heightToDp('18%'),
+                justifyContent: 'space-between',
+            }}>
+                <TextInput 
+                keyboardType='email-address'
+                placeholder= "Email"
+                style={{
+                    height: heightToDp('8%'),
+                    width: widthToDp('70%'),
+                    borderRadius: 100,
+                    backgroundColor: colours.grey,
+                    paddingHorizontal: widthToDp('6%'),
+                    fontFamily: "PoppinsRegular",
+                    fontSize: widthToDp('4%'),
+                }}
+                />
+                <TextInput 
+                autoCompleteType='off'
+                placeholder= "Password"
+                textContentType='password'
+                secureTextEntry={true}
+                style={{
+                    height: heightToDp('8%'),
+                    width: widthToDp('70%'),
+                    borderRadius: 100,
+                    backgroundColor: colours.grey,
+                    paddingHorizontal: widthToDp('6%'),
+                    fontFamily: "PoppinsRegular",
+                    fontSize: widthToDp('4%'),
+                }}
+                />
+            </View>
+            <View style={{
+                height: heightToDp('20%'),
+                justifyContent: 'space-around'
+            }}>
+                <Button 
+                colour = {colours.primary}
+                text = {"LOGIN"}
+                height = {heightToDp('8%')}
+                width = {widthToDp('55%')}
+                onPress = {navigateLogin}
+                />
+            </View>
+            <Text style={{
+              fontFamily: "PoppinsSemiBold",
+              fontSize: widthToDp('4%'),
+              color: colours.text ,
+              textAlign: 'center',
+              marginTop: heightToDp('2%')
+            }}>OR LOGIN WITH</Text>
+            <View style={{
+                flexDirection: 'row',
+                width: widthToDp('25%'),
+                justifyContent: 'space-between',
+            }}>
+                <TouchableOpacity style={{
+                    width: widthToDp('10%'),
+                    height: widthToDp('10%'),
+                    backgroundColor: colours.grey,
+                    borderRadius: 100,
+                    justifyContent: 'center',
+                    alignItems: 'center',
+                }}
+                onPress={() => Alert.alert("Alert", "This feature is not avilable in beta")}>
+                    <AntDesign name="google" size={widthToDp('6%')} color="black" />
+                </TouchableOpacity>
+                <TouchableOpacity style={{
+                    width: widthToDp('10%'),
+                    height: widthToDp('10%'),
+                    backgroundColor: colours.grey,
+                    borderRadius: 100,
+                    justifyContent: 'center',
+                    alignItems: 'center',
+                }}
+                onPress={() => Alert.alert("Alert", "This feature is not avilable in beta")}>
+                    <FontAwesome5 name="facebook-f" size={widthToDp('5%')} color="black" />
+                </TouchableOpacity>
+            </View>
+        </View>
+    );
+}
+
+export default Landing;
