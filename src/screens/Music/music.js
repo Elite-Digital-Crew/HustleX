5ba6cf83ff7fd857a3c46ecdce7435ebc2db5be0
ChinmayX <chinmaysjr26@gmail.com>
2021-05-04 14:49:56 +0530

    music done

--- src/screens/Music/music.js
+++ src/screens/Music/music.js
@@ -1,11 +1,13 @@
 import React from 'react';
-import { Text, View, TouchableOpacity, TextInput, SafeAreaView, StatusBar, ScrollView, Linking, Image } from 'react-native';
+import { Text, View, TouchableOpacity, TextInput, FlatList, SafeAreaView, StatusBar, ScrollView, Linking, Image } from 'react-native';
 import { WebView } from 'react-native-webview';
 
 import Hedder from '../../components/hedder';
 
 import colours from '../../assets/colours/colours';
 import { heightToDp, widthToDp } from '../../Utils';
+import { playlistData, musicData} from '../../assets/data/musicData';
+
 
 const Music = ({ navigation }) => {
     return (
@@ -16,30 +18,45 @@ const Music = ({ navigation }) => {
         }}>
             <StatusBar translucent backgroundColor="transparent" barStyle="dark-content" />
             <Hedder navigation={navigation}/>
+            <ScrollView style={{
+                flex: 1
+            }}>
+                <View>
+                    <Text style={{
+                        color: colours.text,
+                        left: '2%',
+                        fontFamily: "PoppinsBold",
+                        fontSize: widthToDp('6%'),
+                    }}>PLAYLISTS</Text>
+                    <FlatList 
+                    data={musicData}
+                    numColumns={2}
+                    renderItem={({item}) => (
+                        <View style={{
+                            width: '50%',
+                            height: widthToDp('35%'),
+                            padding: 4,
+                            paddingHorizontal: 6
+                        }}>
+                            {item.link}
+                        </View>
+                    )}/>
+                </View>
+            </ScrollView>
 
-            <WebView 
-            source={{html: '<iframe src="https://open.spotify.com/embed/track/7fBv7CLKzipRk6EC6TWHOB" width="500" height="500" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>'}}
-            />
             
-            <WebView 
-            source={{html: '<iframe src="https://open.spotify.com/embed/track/0VjIjW4GlUZAMYd2vXMi3b" width="300" height="380" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>'}}
-            />
+            {/* <WebView source={{html: '<iframe src="https://open.spotify.com/embed/playlist/37i9dQZF1DWUSyphfcc6aL" width="300" height="380" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>'}}/>
+            <WebView source={{html: '<iframe src="https://open.spotify.com/embed/playlist/37i9dQZF1DX70RN3TfWWJh" width="300" height="380" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>'}}/>
+            <WebView source={{html: '<iframe src="https://open.spotify.com/embed/playlist/37i9dQZF1DWSJHnPb1f0X3" width="300" height="380" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>'}}/>
+            <WebView source={{html: '<iframe src="https://open.spotify.com/embed/playlist/37i9dQZF1DX9uKNf5jGX6m" width="300" height="380" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>'}}/>
+            <WebView source={{html: '<iframe src="https://open.spotify.com/embed/playlist/37i9dQZF1DX9BXb6GsGCLl" width="300" height="380" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>'}}/>
 
-            <WebView 
-            source={{html: '<iframe src="https://open.spotify.com/embed/track/0VjIjW4GlUZAMYd2vXMi3b" width="300" height="380" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>'}}
-            />
+            <WebView source={{html: '<iframe src="https://open.spotify.com/embed/playlist/37i9dQZF1DWUI1rlvkdQnb" width="300" height="380" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>'}}/>
+           
+            <WebView source={{html: '<iframe src="https://open.spotify.com/embed/playlist/37i9dQZF1DWXx3Txis2L4x" width="300" height="380" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>'}}/>
 
-            <WebView 
-            source={{html: '<iframe src="https://open.spotify.com/embed/track/0VjIjW4GlUZAMYd2vXMi3b" width="300" height="380" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>'}}
-            />
-
-            <WebView 
-            source={{html: '<iframe src="https://open.spotify.com/embed/track/0VjIjW4GlUZAMYd2vXMi3b" width="300" height="380" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>'}}
-            />
+            <WebView source={{html: '<iframe src="https://open.spotify.com/embed/playlist/37i9dQZF1DX0wiundViT27" width="300" height="380" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>'}}/> */}
             
-            <WebView 
-            source={{html: '<iframe src="https://open.spotify.com/embed/track/0VjIjW4GlUZAMYd2vXMi3b" width="300" height="380" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>'}}
-            />
 
         </SafeAreaView>
     );
