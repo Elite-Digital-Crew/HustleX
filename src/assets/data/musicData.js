a28f14b8e25a8245a5fb31a1b7e0b6a1c1185615
ChinmayX <chinmaysjr26@gmail.com>
2021-05-04 21:00:22 +0530

    music updates

--- src/assets/data/musicData.js
+++ src/assets/data/musicData.js
@@ -26,14 +26,6 @@ export const playlistData = [
         id: 6,
         link: <WebView source={{html: '<iframe src="https://open.spotify.com/embed/playlist/37i9dQZF1DWUI1rlvkdQnb" width=100% height=550 frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>'}}/>,
     },
-    {
-        id: 7,
-        link: <WebView source={{html: '<iframe src="https://open.spotify.com/embed/playlist/37i9dQZF1DWXx3Txis2L4x" width=100% height=550 frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>'}}/>,
-    },
-    {
-        id: 8,
-        link: <WebView source={{html: '<iframe src="https://open.spotify.com/embed/playlist/37i9dQZF1DX0wiundViT27" width=100% height=550 frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>'}}/>,
-    },
 ]
 
 export const musicData = [
@@ -60,18 +52,6 @@ export const musicData = [
     {
         id: 6,
         link: <WebView source={{html: '<iframe src="https://open.spotify.com/embed/track/0yQIhApPPxaPzsgy7FIGnA" width="46%" height=100% frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>'}}/>,
-    },
-    {
-        id: 7,
-        link: <WebView source={{html: '<iframe src="https://open.spotify.com/embed/track/4918TwZfPrxk8vKhuaqIiq" width="46%" height=100% frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>'}}/>,
-    },
-    {
-        id: 8,
-        link: <WebView source={{html: '<iframe src="https://open.spotify.com/embed/track/2ab4xGVMBb14NpPrj4xDq5" width="46%" height=100% frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>'}}/>,
-    },
-    {
-        id: 9,
-        link: <WebView style={{width: '100%'}} source={{html: '<iframe src="https://open.spotify.com/embed/track/16ih0TTm4REIQuYBMPTHm8" width="46%" height=100% frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>'}}/>,
     },
 ]
 
